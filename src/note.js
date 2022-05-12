06f39bb5a1d5b1de72627bb591e32fb7e6642f68
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-05-12 22:43:14 +0600

    adding some planning

--- src/note.js
+++ src/note.js
@@ -1,5 +1,6 @@
 /* 
 This is the note created for keeping important info. 
 this is second day i am practicing this project 
+tomorrow will be the big day practicing the whole project inshaAllah
 
 */

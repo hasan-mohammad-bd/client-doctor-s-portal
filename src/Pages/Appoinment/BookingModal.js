2fba9e27616ec5757ccad04bd86c8cea98bf9b24
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-05-14 21:44:02 +0600

    fixcing navbar

--- src/Pages/Appoinment/BookingModal.js
+++ src/Pages/Appoinment/BookingModal.js
@@ -1,12 +1,20 @@
 import { format } from "date-fns";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const BookingModal = ({date, treatment, setTreatment}) => {
     const {name, slots} = treatment;
+    const [user] = useAuthState(auth);
 
     const handleBooking = event => {
         event.preventDefault();
+        const date = event.target.date.value
         const slot = event.target.slot.value;
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const phone = event.target.phone.value;
+        console.log(slot, name, email, phone, date);
         setTreatment(null)
     }
   return (
@@ -19,14 +27,14 @@ const BookingModal = ({date, treatment, setTreatment}) => {
             Booking for: {name}
           </h3>
             <form onSubmit={handleBooking} className="grid grid-cols-1 gap-3 justify-items-center mt-3">
-            <input type="text" disabled value={format(date,'PP')} class="input input-bordered w-full max-w-xs" />
+            <input type="text" name="date" disabled value={format(date,'PP')} class="input input-bordered w-full max-w-xs" />
             <select name="slot" class="select select-bordered w-full max-w-xs text-black">
             {
                 slots.map(slot => <option value={slot}>{slot}</option>)
             }
             </select>
-            <input type="text" name="name" placeholder="Your Name" class="input input-bordered w-full max-w-xs" />
-            <input type="email" name="email" placeholder="Your Email" class="input input-bordered w-full max-w-xs" />
+            <input type="text" name="name" disabled value={user?.displayName} placeholder="Your Name" class="input input-bordered w-full max-w-xs" />
+            <input type="email" name="email" disabled value={user?.email} placeholder="Your Email" class="input input-bordered w-full max-w-xs" />
             <input type="text" name="phone" placeholder="Your Phone" class="input input-bordered w-full max-w-xs" />
             <input type="submit" value="Submit" class=" w-full max-w-xs btn btn-secondary" />
             </form>

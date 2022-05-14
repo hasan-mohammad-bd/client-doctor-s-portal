b61cb8b540b4ed768f72a007d6007588cb48b584
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-05-14 14:48:35 +0600

    adding modal

--- src/Pages/Appoinment/AvaliableAppoinment.js
+++ src/Pages/Appoinment/AvaliableAppoinment.js
@@ -1,12 +1,30 @@
-import { format } from 'date-fns';
-import React from 'react';
+import { format } from "date-fns";
+import React, { useEffect, useState } from "react";
+import BookingModal from "./BookingModal";
+import Service from "./Service";
 
-const AvaliableAppoinment = ({date}) => {
-    return (
-        <div>
-                    <p className='text-blue-400 text-center'>You have selected: {format(date, 'PP')}</p>
-        </div>
-    );
+const AvaliableAppoinment = ({ date }) => {
+    const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState(null)
+
+    useEffect(()=>{
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
+  return (
+    <div>
+      <p className="text-secondary text-center">
+        You have selected: {format(date, "PP")}
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+          {
+              services.map(service => <Service service={service} key={service._id} setTreatment={setTreatment}></Service>)
+          }
+      </div>
+      {treatment && <BookingModal date={date} treatment={treatment} setTreatment={setTreatment}></BookingModal>}
+    </div>
+  );
 };
 
 export default AvaliableAppoinment;

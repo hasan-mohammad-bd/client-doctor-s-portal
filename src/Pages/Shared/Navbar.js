b3ceab131cc6471463e0fdf07b5d25c7087e87d4
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-05-16 01:44:33 +0600

    adding jwt in booking

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -10,6 +10,7 @@ const Navbar = () => {
 
     const logout = () => {
         signOut(auth);
+        localStorage.removeItem('accessToken')
       };
 
     const menuItems = <>
@@ -24,7 +25,8 @@ const Navbar = () => {
         <li>{user ? <button className="btn btn-ghost"  onClick={logout} >Sign Out</button> : <Link to="/login">Login</Link>}</li>
     </>
     return (
-        <div className="navbar flex justify-between">
+      <div>
+                <div className="navbar flex justify-between">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex="0" className="btn btn-ghost lg:hidden">
@@ -41,12 +43,14 @@ const Navbar = () => {
                     {menuItems}
                 </ul>
             </div>
-            <div className='navbar-end'>
+
+        </div>
+        <div className='navbar-end inline-block'>
             <label tabIndex="0" htmlFor="deshboard-sidebar" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
             </div>
-        </div>
+      </div>
     );
 };
 

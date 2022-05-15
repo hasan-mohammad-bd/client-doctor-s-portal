bce563f85ed3556e9f7e294931dca5b388b22a07
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-05-15 16:22:20 +0600

    adding deshboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Pages/About/About";
@@ -8,22 +7,43 @@ import Login from "./Pages/Login/Login";
 import SignUp from "./Pages/Login/SignUp";
 import FooterNav from "./Pages/Shared/FooterNav";
 import Navbar from "./Pages/Shared/Navbar";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import RequireAuth from "./Pages/Login/RequireAuth";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import MyAppointment from "./Pages/Dashboard/MyAppointment";
+import Review from "./Pages/Home/Review";
+import MyReview from "./Pages/Dashboard/MyReview";
 
 function App() {
   return (
     <div className="App container mx-auto">
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/about" element={<About></About>}></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <Route path="/signup" element={<SignUp></SignUp>}></Route>
-          <Route path="/appointment" element={<Appoinment></Appoinment>}></Route>
-        </Routes>
-        <ToastContainer/>
-        <FooterNav></FooterNav>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="/appointment" element=
+        {
+            <RequireAuth>
+              <Appoinment></Appoinment>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/dashboard" element=
+        {
+            <RequireAuth>
+              <Dashboard></Dashboard>
+            </RequireAuth>
+          }
+        >
+          <Route index element={<MyAppointment></MyAppointment>}></Route>
+          <Route path='review' element={<MyReview></MyReview>}></Route>
+        </Route>
+      </Routes>
+      <ToastContainer />
+      <FooterNav></FooterNav>
     </div>
   );
 }
